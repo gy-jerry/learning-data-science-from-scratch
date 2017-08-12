23821d7efcee0587b78ba6fe3d0c3c8dc7fe6427
gy-jerry <920680270@qq.com>
2017-08-12 15:33:15 +0800

    test commit with pycharm

--- data_science_from_scratch/visualization.py
+++ data_science_from_scratch/visualization.py
@@ -1,4 +1,5 @@
-#coding:utf-8
+# 中文编码
+# coding:utf-8
 
 # 第3章 可视化数据
 
@@ -6,8 +7,12 @@
 
 from matplotlib import pyplot as plt
 
-plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
-plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+# settings
+# 用来正常显示中文标签
+plt.rcParams['font.sans-serif']=['SimHei']
+# 用来正常显示负号
+plt.rcParams['axes.unicode_minus']=False
+
 
 # 线图
 years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
@@ -21,3 +26,19 @@ plt.show()
 
 
 # 条形图
+movies = ["Annie Hall", "Ben-Hur", "Casablanca", "Gandhi", "West Side Story"]
+num_oscars = [5, 11, 3, 8, 10]
+
+# bars are by default width 0.8, so we'll add 0.1 to the left coordinates
+# so that each bar is centered
+xs = [i + 0.1 for i, _ in enumerate(movies)]
+
+# plot bars with left x-coordinates [xs], heights [num_oscars]
+plt.bar(xs, num_oscars)
+plt.ylabel("# of Academy Awards")
+plt.title("My Favorite Movies")
+
+# label x-axis with movie names at bar centers
+plt.xticks([i + 0.5 for i, _ in enumerate(movies)], movies)
+
+plt.show()
